37904bc94673e1edfd1dadcef9368d806b73ea52
KC Juntunen <juntunenkc@lan.amstore.com>
2018-02-22 09:12:59 -0500

    Handle err.

---
+++ FormatFixtureBook/PDFCreatorFileOpenException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FormatFixtureBook {
+	[Serializable]
+	public class PDFCreatorFileOpenException : Exception {
+		public PDFCreatorFileOpenException() { }
+		public PDFCreatorFileOpenException(string message) : base(message) { }
+		public PDFCreatorFileOpenException(string message, Exception inner) : base(message, inner) { }
+		protected PDFCreatorFileOpenException(
+		System.Runtime.Serialization.SerializationInfo info,
+		System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+}
